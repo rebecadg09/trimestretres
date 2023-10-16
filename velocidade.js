4da57fb7bc3e9d5d096cad7ede404f48962af549
rebecadg09 <113352593+rebecadg09@users.noreply.github.com>
2023-10-16 08:11:10 -0300

    161023

---
+++ velocidade.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const permitida = Number(form.inPermitida.value)
+    const condutor = Number(form.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
+
+    if(condutor <= permitida){
+        resp.innerText = `Situação: Multa Leve`
+    
+    } else if(condutor <= leve) {
+        resp.innerText = `Situação: Multa Leve`
+    } else {
+        resp.innerText = `Situação: Multa Grave`
+    }
+})
